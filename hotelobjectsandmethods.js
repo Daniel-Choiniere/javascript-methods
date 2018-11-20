6d3e5d8c6f96f7f808f49f4270f3b16f8eeb1c23
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2018-11-20 01:44:36 +0000

    working bookRoom method

--- hotelobjectsandmethods.js
+++ hotelobjectsandmethods.js
@@ -3,22 +3,30 @@ var hotel = {
     rating: 5.0,
     numberOfRooms: 5,
     roomRate: 325.00,
-    roomNumbersAvailable: ["102", "103", "104", "105", "106"],
-    roomNumbersBooked: ["101"],
+    roomNumbersAvailable: ["101", "102", "103", "104", "105", "106"],
+    roomNumbersBooked: [],
     roomType: "Queen",
     
     numberOfRoomsAvailable: function() {
+        // length of the number of rooms available, returned as an integer (1,5,20, etc)
         return this.roomNumbersAvailable.length;
     },
     
     numberOfRoomsBooked: function() {
         return this.roomNumbersBooked.length;
-    }
+    },
     
     numberOfRooms: function() {
         return this.roomNumbersAvailable.length + this.roomNumbersBooked.length;
     },
     
+    bookRoom: function() {
+        if (this.numberOfRoomsAvailable() > 0) {
+            // select a random avaliable room
+            var randomRoom = this.roomNumbersAvailable[Math.floor(Math.random() * this.roomNumbersAvailable.length)];
+            this.roomNumbersAvailable.splice(this.roomNumbersAvailable.indexOf(randomRoom), 1);
+            this.roomNumbersBooked.push(randomRoom);
+        }
+    }
 };
-
 
