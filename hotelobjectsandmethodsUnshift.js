a6caef61009b6df55eb92a2327618179740c471f
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2018-11-27 00:02:19 +0000

    user response for book and checkout functions

--- hotelobjectsandmethodsUnshift.js
+++ hotelobjectsandmethodsUnshift.js
@@ -20,6 +20,8 @@ var hotel = {
         return this.roomNumbersAvailable.length + this.roomNumbersBooked.length;
     },
     
+    // method to book a random room
+    
     bookRoom: function() {
         if (this.numberOfRoomsAvailable() > 0) {
             // select a random avaliable room
@@ -31,10 +33,13 @@ var hotel = {
         }
     },
     
+    // method to check out a random (already booked) room
     checkoutRoom: function() {
         if (this.roomNumbersBooked.length > 0) {
             var randomRoomCheckout = this.roomNumbersBooked[Math.floor(Math.random() * this.roomNumbersBooked.length)];
             this.roomNumbersAvailable.unshift(this.roomNumbersBooked.splice(this.roomNumbersBooked.indexOf(randomRoomCheckout), 1)[0]);
+        } else {
+        console.log("There are currently no rooms booked")
         }
     }
 };
